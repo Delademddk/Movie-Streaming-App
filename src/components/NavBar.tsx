5dac8dce62f07d701b66c47ccc3d701b0e6cb83c
Delademddk <kegudeladem@gmail.com>
2026-04-07 09:44:02 +0000

    feat: initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <div>
+      <div>
+        <h2>Movie Explorer</h2>
+
+        <p>Home</p>
+        <p>Movies</p>
+        <p>TV shows</p>
+        <p>My LIst</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
