4db223d60924584801f4aa32f9f316b91913db55
Delademddk <kegudeladem@gmail.com>
2026-04-08 23:03:53 +0000

    Added MovieCard component

---
+++ src/app/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar";
+import MovieCard from "@/components/MovieCard";
+
+export default function HomePage() {
+
+
+  return (
+    <div>
+      <NavBar />
+      <MovieCard/>
+    </div>
+  );
+}
